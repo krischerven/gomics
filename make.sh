d839ffc07cc55a0687253a4f264b81a101ba47fc
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-05-03 14:46:39 -0400

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-cd src
+mkdir -p /tmp/gomics
+sudo cp -r "$PWD/." /tmp/gomics
+odir="$PWD"
+cd /tmp/gomics/src
+GOPATH="$PWD" # for dependencies
 go generate
 go build
 mkdir -p ../bin
 mv src ../bin/gomics
+cd "$odir"
+cp /tmp/gomics/bin/gomics "$PWD/bin"
